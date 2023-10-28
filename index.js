9e375174170da581e0397afb2bf3ea099e020ee2
JordanDorsey <jordandorsey04@gmail.com>
2023-10-28 10:51:34 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, Name) {
+    return drivers.filter(driver => driver.toUpperCase() === Name.toUpperCase());
+} 
+
+console.log(findMatching())
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver[0] === name[0])
+}
+
+console.log(fuzzyMatch)
+
+function matchName(drivers, name) {
+const matches = drivers.filter(driver => driver.name === name);
+    return matches
+}
